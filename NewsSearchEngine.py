24385aa812797adb3b0c0d989538311dc0f5bf15
Noble-1206 <seunghk1206@gmail.com>
2021-03-27 07:55:13 +0900

    Update NewsSearchEngine.py

--- NewsSearchEngine.py
+++ NewsSearchEngine.py
@@ -1,7 +1,8 @@
 import bs4 as bs
 import urllib.request
+import itertools
 
-search_ls = ["https://munfinder.com"]
+search_ls = ["https://munfinder.com", "https://internshala.com/internships"]
 nameSearch = ['munfinder']
 tempL = []
 for Ind, each in enumerate(search_ls):
@@ -10,10 +11,12 @@ for Ind, each in enumerate(search_ls):
     for url in soup.find_all('a'):
         tempurl = url.get('href')
         try: 
-            if nameSearch[Ind] in tempurl:
-                if Ind == 0:
+            if Ind == 0:
+                if nameSearch[Ind] in tempurl:
                     if 'https://munfinder.com/mun/' in tempurl:
                         tempL.append(tempurl.split('/')[4])
+            elif Ind == 1:
+                tempL.append(url.get_text())
         except:
             pass
 file = open("./Data.txt", "w") #[username_info].txt - > youngho.txt, file의 권한 = w -> Write
