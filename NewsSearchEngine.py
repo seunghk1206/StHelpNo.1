06be544e6784b4ff861bec7c6225f45b0549736e
Noble-1206 <seunghk1206@gmail.com>
2021-03-27 07:06:16 +0900

    Update NewsSearchEngine.py

--- NewsSearchEngine.py
+++ NewsSearchEngine.py
@@ -1,6 +1,19 @@
 import bs4 as bs
 import urllib.request
 
-source = urllib.request.urlopen('https://munfinder.com/mun/india-model-united-nations-aimun/').read()
-search_ls = ["https://munfinder.com/mun/india-model-united-nations-aimun/", "summer programs - university"]
-soup = bs.BeautifulSoup(source,'lxml')
+search_ls = ["https://munfinder.com"]
+nameSearch = ['munfinder']
+tempL = []
+for Ind, each in enumerate(search_ls):
+    source = urllib.request.urlopen(each).read()
+    soup = bs.BeautifulSoup(source,'lxml')
+    for url in soup.find_all('a'):
+        tempurl = url.get('href')
+        try: 
+            if nameSearch[Ind] in tempurl:
+                if Ind == 0:
+                    if 'https://munfinder.com/mun/' in tempurl:
+                        tempL.append(tempurl.split('/')[4])
+        except:
+            pass
+print(set(tempL))
