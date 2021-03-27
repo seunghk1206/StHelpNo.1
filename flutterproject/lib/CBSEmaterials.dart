783f525a54a460ee25b60543feaf9f5b833aa694
mmutyampeta <mmutyampeta@gmail.com>
2021-03-27 15:06:51 +0530

    new

--- flutterproject/lib/CBSEmaterials.dart
+++ flutterproject/lib/CBSEmaterials.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+// ignore: camel_case_types
+class cbseMaterials extends StatelessWidget {
+  _launchURLcbseWebsite() async {
+    const url = 'https://www.cbse.gov.in/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Text(
+            "CBSE stands for Central Board of Secondary Education.  CBSE is one of the most popular boards in India"),
+        ElevatedButton(
+          child: Text("The CBSE official website"),
+          onPressed: _launchURLcbseWebsite,
+        ),
+        ElevatedButton(),
+      ],
+    ));
+  }
+}
