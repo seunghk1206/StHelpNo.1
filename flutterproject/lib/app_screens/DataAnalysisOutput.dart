bd0c2e6df953adba306723b09a41e8df58a7124f
mmutyampeta <mmutyampeta@gmail.com>
2021-03-28 02:10:49 +0530

    Hey

--- flutterproject/lib/app_screens/DataAnalysisOutput.dart
+++ flutterproject/lib/app_screens/DataAnalysisOutput.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
 
-void main() {
-  var splitL = Analysis("Data.txt");
-  print(splitL);
-}
-
-Analysis(var filename) {
-  return File(filename).readAsString().then((String contents) {
-    var splitL = contents.split(", ");
-    print(splitL);
-  });
+class AnalysisClass {
+  // ignore: non_constant_identifier_names
+  Analysis(var filename) {
+    var splitList;
+    File(filename).readAsString().then((String contents) {
+      splitList = contents.split(", ");
+      return (splitList);
+    });
+  }
 }
