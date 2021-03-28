6cbe74f053d499dd3c82772cf035382e73f987f3
mmutyampeta <mmutyampeta@gmail.com>
2021-03-29 00:09:54 +0530

    New

--- flutterproject/lib/app_screens/StudyMaterials.dart
+++ flutterproject/lib/app_screens/StudyMaterials.dart
@@ -44,202 +44,170 @@ class StudyMaterials extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.primaryColor,
-
-      body: ListView(
-        
-        children: <Widget>[
-           Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-          Text(
-                "Study Materials for Students",
+      body: ListView(children: <Widget>[
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        Text(
+          "Study Materials for Students",
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontFamily: "Montserrat",
+          ),
+        ),
+        Padding(padding: EdgeInsets.only(bottom: 60.0)),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: ElevatedButton(
+              child: Text(
+                "ICSE",
                 textAlign: TextAlign.center,
                 style: const TextStyle(
-                  fontSize: 25,
+                  fontSize: 20,
                   color: Colors.white,
-                  fontWeight: FontWeight.w600,
+                  fontWeight: FontWeight.w400,
                   fontFamily: "Montserrat",
                 ),
               ),
-            
-            Padding(
-                    padding: EdgeInsets.only(bottom: 60.0)),
-          SizedBox(
-            width:50,
-            height:50,
-            child:ElevatedButton(
-                    child: Text(
-                  "ICSE",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateToICSE(context);
+              }),
+        ),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        SizedBox(
+            width: 50,
+            height: 50,
+            child: ElevatedButton(
+              child: Text(
+                "CBSE",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
                 ),
-                style: ElevatedButton.styleFrom(
-      primary: Colors.blue,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                    onPressed: () {
-                      navigateToICSE(context);
-                    }),
-          ),
-
-                     Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-
-            SizedBox(
-            width:50,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateToCBSE(context);
+              },
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        SizedBox(
+            width: 50,
             height: 50,
-            child:ElevatedButton(
-                  child: Text(
-                  "CBSE",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
+            child: ElevatedButton(
+              child: Text(
+                "Cambridge International",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
                 ),
-                style: ElevatedButton.styleFrom(
-      primary: Colors.red,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                  onPressed: () {
-                    navigateToCBSE(context);
-                  },
-                )),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-
-                SizedBox(
-            width:50,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateTocie(context);
+              },
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        SizedBox(
+            width: 50,
             height: 50,
-            child:ElevatedButton(
-                  child: Text(
-                  "Cambridge International",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
-                ),style: ElevatedButton.styleFrom(
-      primary: Colors.orange,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                  onPressed: () {
-                    navigateTocie(context);
-                  },
-                )),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-
-                SizedBox(
-            width:50,
-            height: 50,
-            child:ElevatedButton(
-                  child: Text(
-                  "IIT-JEE",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
+            child: ElevatedButton(
+              child: Text(
+                "IIT-JEE",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
                 ),
-                style: ElevatedButton.styleFrom(
-      primary: Colors.green,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                  onPressed: () {
-                    navigateToJEE(context);
-                  },
-                )),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-                
-
-                SizedBox(
-            width:50,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateToJEE(context);
+              },
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        SizedBox(
+            width: 50,
             height: 50,
-            child:ElevatedButton(
-                  child: Text(
-                  "SAT",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
+            child: ElevatedButton(
+              child: Text(
+                "SAT",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
                 ),
-                style: ElevatedButton.styleFrom(
-      primary: Colors.pink,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                  onPressed: () {
-                    navigateToSAT(context);
-                  },
-                )),
-                
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-                
-
-
-                SizedBox(
-            width:50,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.pink,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateToSAT(context);
+              },
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        SizedBox(
+            width: 50,
             height: 50,
-            child:ElevatedButton(
-                  child: Text(
-                  "International Baccalaureate (IB)",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Montserrat",
-                  ),
-                ),style: ElevatedButton.styleFrom(
-      primary: Colors.purple,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(70.0)),
-    ),
-                  onPressed: () {
-                    navigateToib(context);
-                  },
-                )),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-                
-
-
-        ]),
+            child: ElevatedButton(
+              child: Text(
+                "International Baccalaureate (IB)",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.purple,
+                onPrimary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)),
+              ),
+              onPressed: () {
+                navigateToib(context);
+              },
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+      ]),
     );
-
   }
 }
