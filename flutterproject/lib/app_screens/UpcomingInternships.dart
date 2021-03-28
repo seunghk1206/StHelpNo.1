83c07c25867f0e655fb101c5e0bd8ad393590160
Noble-1206 <seunghk1206@gmail.com>
2021-03-28 18:13:28 +0900

    Update UpcomingInternships.dart

--- flutterproject/lib/app_screens/UpcomingInternships.dart
+++ flutterproject/lib/app_screens/UpcomingInternships.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UpcomingInternships extends StatelessWidget {
+  final Future<String> textList = File("Data.txt").readAsString();
   @override
   Widget build(BuildContext context) {
+    String initS = textList.toString();
+    var splitL = initS.split(", ");
     return Scaffold(
         appBar: AppBar(
       title: Text(
