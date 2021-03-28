530a835aff0a0e187585173de6616219719b6bc1
Shreyes Sridhara <shreyesXsridhara@gmail.com>
2021-03-29 00:56:11 +0530

    Update upcomingEvents.dart

--- flutterproject/lib/app_screens/upcomingEvents.dart
+++ flutterproject/lib/app_screens/upcomingEvents.dart
@@ -15,6 +15,7 @@ class UpcomingEvents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     var events = {
       1: "Colloquium MUN",
       2: "Brainiac MUN",
@@ -28,220 +29,160 @@ class UpcomingEvents extends StatelessWidget {
       10: "International Crisis Summit MUN 2021"
     };
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Upcoming MUN in India",
+      backgroundColor: Colors.orange,
+    
+        body: ListView(
+          
+          children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 50.0)),
+
+              Text("Upcoming Events in India",
             textAlign: TextAlign.center,
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 10.0)),
+                style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Montserrat",
+          ),),
+
+            Padding(padding: EdgeInsets.only(top: 30.0,)),
+                    Text("MUNs:",
+                    textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "Montserrat",
+          ),),
+              
+              Padding(padding: EdgeInsets.only(bottom: 40.0)),
               Text(
                 events[1],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[2],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[3],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[4],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[5],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[6],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[7],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[8],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[9],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Padding(padding: EdgeInsets.only(bottom: 20.0)),
               Text(
                 events[10],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
                 ),
               ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
-              ElevatedButton(
-                child: Text(
-                  "Click to view more upcoming MUN conferences in India",
-                  textAlign: TextAlign.center,
-                ),
-                onPressed: _launchURLmun,
-              ),
+              Padding(padding: EdgeInsets.only(bottom: 50.0)),
+              Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          alignment: Alignment.bottomLeft,
+                          icon: Icon(Icons.add),
+                          iconSize: 60,
+                          color: Colors.green,
+                          onPressed: _launchURLmun,
+                        )
+                      ],
+                    )),
               Padding(padding: EdgeInsets.only(bottom: 30.0)),
             ],
           ),
-        ));
+        );
   }
 }
-
-/*
-class EventState extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return UpcomingEvents();
-  }
-}
-*/
-/*
-class UpcomingEvents extends State<EventState> {
-  var _eventIndex = 1;
-
-  void _toggleEvent() {
-    setState(() {
-      _eventIndex = _eventIndex + 5;
-    });
-  }
-
-  final textList = File("Data.txt").readAsString();
-  _launchURLmun() async {
-    const url = 'https://munfinder.com';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String initS = textList.toString();
-    var splitL = initS.split(", ");
-    var events = {
-      1: "Colloquium MUN",
-      2: "Brainiac MUN",
-      3: "Delhi School of Economics MUN",
-      4: "8th World Congress on HMS",
-      5: "Stay Home Model United Nations",
-      6: "Longfellow Middle School MUN Conference",
-      7: "Western Canada High School MUN 2021",
-      8: "elbMUN Online Conference 2021",
-      9: "Stay At Home Model United Nations 2021",
-      10: "International Crisis Summit MUN 2021"
-    };
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Upcoming MUN in India",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              Text(
-                events[_eventIndex],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
-              Text(
-                events[_eventIndex + 1],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
-              Text(
-                events[_eventIndex + 2],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
-              Text(
-                events[_eventIndex + 3],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 5.0)),
-              Text(
-                events[_eventIndex + 4],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 10.0)),
-              ElevatedButton(
-                child: Text(
-                  "Click to view more upcoming MUN conferences in India",
-                  textAlign: TextAlign.center,
-                ),
-                onPressed: _launchURLmun,
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 30.0)),
-              ElevatedButton(
-                child: Text("Load more Upcoming MUN conferences"),
-                onPressed: _toggleEvent,
-              ),
-            ],
-          ),
-        ));
-  }
-}
-*/
