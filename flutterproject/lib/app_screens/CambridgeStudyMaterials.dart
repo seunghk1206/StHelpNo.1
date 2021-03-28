fd0c5ee5e4a89ee75ff66fa6cd4cf13f1779e732
mmutyampeta <mmutyampeta@gmail.com>
2021-03-28 16:23:24 +0530

    New

--- flutterproject/lib/app_screens/CambridgeStudyMaterials.dart
+++ flutterproject/lib/app_screens/CambridgeStudyMaterials.dart
@@ -32,7 +32,8 @@ class CambridgeContainer extends StatelessWidget {
   }
 
   _launchURLciePast() async {
-    const url = 'https://www.physicsandmathstutor.com/';
+    const url =
+        'https://pastpapers.papacambridge.com/?dir=Cambridge%20International%20Examinations%20%28CIE%29';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -40,102 +41,74 @@ class CambridgeContainer extends StatelessWidget {
     }
   }
 
- 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         backgroundColor: Colors.orange,
-              actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            Icons.menu_book,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.menu_book,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+        title: Text(
+          "Cambridge International",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: ListView(children: <Widget>[
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        Text(
+          "The Cambridge International Curriculum",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: "Montserrat",
+            fontWeight: FontWeight.w600,
             color: Colors.white,
           ),
-          onPressed: () {
-            // do something
-          },
-        )
-      ],
-          title: Text(
-            "Cambridge International", 
-            textAlign: TextAlign.center,
-            
+        ),
+        Padding(padding: EdgeInsets.only(bottom: 60.0)),
+        ElevatedButton(
+            child: Text("The official Cambridge International Website"),
+            onPressed: _launchURLCambridgeofficial,
+            //style: const Styles.buttonTextStyle,
+            style: ElevatedButton.styleFrom(
+              primary: Colors.orange,
+              onPrimary: Colors.white,
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)),
+            )),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        ElevatedButton(
+          child: Text("A level Chemistry Revision Material"),
+          onPressed: _launchURLcieChem,
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orange,
+            onPrimary: Colors.white,
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(30.0)),
           ),
         ),
-
-      body: ListView(
-        
-        children: <Widget>[
-           Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-          Text(
-              "The Cambridge International Curriculum",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: "Montserrat",
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
-            
-            Padding(
-                    padding: EdgeInsets.only(bottom: 60.0)),
-
-
-
-          ElevatedButton(
-              child: Text("The official Cambridge International Website"),
-              onPressed: _launchURLCambridgeofficial,
-              //style: const Styles.buttonTextStyle,
-            style: ElevatedButton.styleFrom(
-      primary: Colors.orange,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(30.0)),
-      )),
-                     Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-
-          ElevatedButton(
-              child: Text("A level Chemistry Revision Material"),
-              onPressed: _launchURLcieChem,
-          
-              style: ElevatedButton.styleFrom(
-      primary: Colors.orange,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(30.0)),
-              ),
-            ),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-
-
-                ElevatedButton(
-              child: Text("A level Physics Study Material"),
-              onPressed: _launchURLciePMT,
-          
-              style: ElevatedButton.styleFrom(
-      primary: Colors.orange,
-      onPrimary: Colors.white,
-      shape: new RoundedRectangleBorder(
-      borderRadius: new BorderRadius.circular(30.0)),
-              ),
-            ),
-                 Padding(
-                    padding: EdgeInsets.only(top: 50.0)),
-
-                
-        ]),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+        ElevatedButton(
+          child: Text("A level Physics Study Material"),
+          onPressed: _launchURLciePMT,
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orange,
+            onPrimary: Colors.white,
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(30.0)),
+          ),
+        ),
+        Padding(padding: EdgeInsets.only(top: 50.0)),
+      ]),
     );
-
   }
 }
-
-
