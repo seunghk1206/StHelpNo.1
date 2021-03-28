723e4aec4ee2d789e9771977eb4a4a110f14cdaf
mmutyampeta <mmutyampeta@gmail.com>
2021-03-28 23:20:33 +0530

    New

--- flutterproject/lib/app_screens/UpcomingInternships.dart
+++ flutterproject/lib/app_screens/UpcomingInternships.dart
@@ -3,6 +3,161 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class InternshipWidget extends StatelessWidget {
+  _launchURLintern() async {
+    const url = 'https://internshala.com/internships/work-from-home-jobs';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _launchURLmpower() async {
+    const url = 'https://forms.gle/QQPFWFzaHjeoFHFZA';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var internships = {
+      1: "Text Correction (Sankrit)",
+      2: "Software Project Management",
+      3: "Business Development (Sales)",
+      4: "Diversity & Inclusion",
+      5: "Graphic Design",
+      6: "Operations",
+      7: "Human Resources (HR) Work",
+      8: "Document Formatting/Editing",
+      9: "Android App Development",
+      10: "CSR Coordination",
+    };
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Upcoming Internship Opportunities",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Internship opportunities available for students.  Note: All the internships mentioned here are virtual",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            Text(
+              "Social Media Team at MPower For Youth",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              child: Text("MPower For Youth Social Media Application Form"),
+              onPressed: _launchURLmpower,
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 30.0)),
+            Text(
+              internships[1],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[2],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[3],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[4],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[5],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[6],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[7],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[8],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[9],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            Text(
+              internships[10],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 5.0)),
+            ElevatedButton(
+              child: Text("Click here to find more internship opportunities"),
+              onPressed: _launchURLintern,
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 30.0)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class InternState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -138,3 +293,4 @@ class InternshipWidget extends State<InternState> {
     );
   }
 }
+*/
--- flutterproject/lib/app_screens/upcomingEvents.dart
+++ flutterproject/lib/app_screens/upcomingEvents.dart
@@ -3,13 +3,144 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class UpcomingEvents extends StatelessWidget {
+  _launchURLmun() async {
+    const url = 'https://munfinder.com';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var events = {
+      1: "Colloquium MUN",
+      2: "Brainiac MUN",
+      3: "Delhi School of Economics MUN",
+      4: "8th World Congress on HMS",
+      5: "Stay Home Model United Nations",
+      6: "Longfellow Middle School MUN Conference",
+      7: "Western Canada High School MUN 2021",
+      8: "elbMUN Online Conference 2021",
+      9: "Stay At Home Model United Nations 2021",
+      10: "International Crisis Summit MUN 2021"
+    };
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Upcoming MUN in India",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 10.0)),
+              Text(
+                events[1],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Text(
+                events[2],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Text(
+                events[3],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Text(
+                events[4],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 5.0)),
+              Text(
+                events[5],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Text(
+                events[6],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Text(
+                events[7],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Text(
+                events[8],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Text(
+                events[9],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              Text(
+                events[10],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              ElevatedButton(
+                child: Text(
+                  "Click to view more upcoming MUN conferences in India",
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: _launchURLmun,
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+            ],
+          ),
+        ));
+  }
+}
+
+/*
 class EventState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return UpcomingEvents();
   }
 }
-
+*/
+/*
 class UpcomingEvents extends State<EventState> {
   var _eventIndex = 1;
 
@@ -113,5 +244,4 @@ class UpcomingEvents extends State<EventState> {
         ));
   }
 }
-
-// class ClassName extends addadsf extends statelesswidget
+*/
