825b95f1d25ddacecf4193f9b88b65d5f0bbf193
mmutyampeta <mmutyampeta@gmail.com>
2021-03-28 13:02:01 +0530

    Update upcomingEvents.dart

--- flutterproject/lib/app_screens/upcomingEvents.dart
+++ flutterproject/lib/app_screens/upcomingEvents.dart
@@ -1,12 +1,46 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class UpcomingEvents extends StatelessWidget {
+class AnalysisClass {
+  // ignore: non_constant_identifier_names
+  Analysis(var filename) {
+    var splitList;
+    File(filename).readAsString().then((String contents) {
+      splitList = contents.split(", ");
+      return (splitList);
+    });
+  }
+}
+
+abstract class Intermediate extends StatelessWidget {
+  // ignore: non_constant_identifier_names
+  Analysis(var filename) {
+    var splitList;
+    File(filename).readAsString().then((String contents) {
+      splitList = contents.split(", ");
+      return (splitList);
+    });
+  }
+}
+
+class UpcomingEvents extends Intermediate {
+  var textList Analysis("Data.txt");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Upcoming MUN in India"),
+        ),
         body: Center(
-      child: Column(),
-    ));
+          child: Column(
+            children: [
+              Text("Below are displayed upcoming Model United Nations ")
+            ],
+          ),
+        ));
   }
 }
+
+// class ClassName extends addadsf extends statelesswidget
