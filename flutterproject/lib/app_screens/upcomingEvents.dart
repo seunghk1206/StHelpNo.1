9f21fb742af3e3e06a26958d953f0b197ae2f84f
Noble-1206 <seunghk1206@gmail.com>
2021-03-28 18:09:39 +0900

    Update upcomingEvents.dart

--- flutterproject/lib/app_screens/upcomingEvents.dart
+++ flutterproject/lib/app_screens/upcomingEvents.dart
@@ -1,13 +1,16 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:core';
 
 // Fix below here
 class UpcomingEvents extends StatelessWidget {
-  final textList = File("Data.txt").readAsString();
+  final Future<String> textList = File("Data.txt").readAsString();
   @override
   Widget build(BuildContext context) {
+    String initS = textList.toString();
+    var splitL = initS.split(", ");
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -19,7 +22,7 @@ class UpcomingEvents extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                "Below are displayed upcoming Model United Nations conferences in India",
+                splitL[0],
                 textAlign: TextAlign.center,
               )
             ],
